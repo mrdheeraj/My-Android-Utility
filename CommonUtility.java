5b82ff64035917180edac8d95bd8bd7d7d45109c
Dheeraj Purohit <kingdheeraj77@gmail.com>
2017-04-08 15:02:20 +0530

    Add files via upload

---
+++ CommonUtility.java
@@ -0,0 +1,60 @@
+package com.mrdheerajpurohit.utility;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Dheeraj Purohit CommonUtility for Android.
+ */
+
+public class CommonUtility {
+
+    public static void share(Context ctx, String sharingMsg, String emailSubject, String title) {
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(Intent.EXTRA_TEXT, sharingMsg);
+        sharingIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+
+        ctx.startActivity(Intent.createChooser(sharingIntent, title));
+    }
+
+    public static void shareImage(Context context, Uri uri, String title) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        shareIntent.setType("image/jpeg");
+        context.startActivity(Intent.createChooser(shareIntent, title));
+    }
+
+    public static boolean isValidEmail(String email) {
+        final String emailPattern = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Matcher matcher;
+        Pattern pattern = Pattern.compile(emailPattern);
+
+        matcher = pattern.matcher(email);
+
+        if (matcher != null)
+            return matcher.matches();
+        else
+            return false;
+    }
+
+    public static boolean isLandscape(Context context) {
+        return context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+    public static boolean isPortrait(Context context) {
+        return context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+    public static boolean isTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+}
---
+++ ConnectionDetectorUtility.java
@@ -0,0 +1,59 @@
+package com.mrdheerajpurohit.utility;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+/**
+ * Dheeraj Purohit ConnectionDetectorUtility for Android.
+ */
+
+public class ConnectionDetectorUtility {
+     
+    public static boolean isConnectingToInternet(Context context){
+        ConnectivityManager connectivity = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+          if (connectivity != null)
+          {
+              NetworkInfo[] info = connectivity.getAllNetworkInfo();
+              if (info != null)
+                  for (int i = 0; i < info.length; i++)
+                      if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                      {
+                          return true;
+                      }
+
+          }
+          return false;
+    }
+
+    public static boolean isWifi(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetInfo != null
+                && activeNetInfo.getType() == ConnectivityManager.TYPE_WIFI;
+    }
+
+    public static boolean is3G(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetInfo != null
+                && activeNetInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+    }
+
+    public static boolean is4G(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetInfo != null && activeNetInfo.isConnectedOrConnecting()) {
+            if (activeNetInfo.getType() == TelephonyManager.NETWORK_TYPE_LTE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
